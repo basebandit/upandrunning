8871552b236e381a00f44c4435ad05058b0561b3
basebandit <evansonmwangi83@gmail.com>
2023-04-03 20:43:46 +0300

    replaces old public_ip output to show the DNS name of the ALB

--- main.tf
+++ main.tf
@@ -252,7 +252,7 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "Public IP address of the web server instance"
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
